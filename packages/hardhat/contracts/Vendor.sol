929c6600d12200efdd3c36a17946df1d684cb41d
Lucas Seijo <lucaseijo7@gmail.com>
2023-04-19 21:27:22 -0300

    challenge finished

--- packages/hardhat/contracts/Vendor.sol
+++ packages/hardhat/contracts/Vendor.sol
@@ -1,23 +1,74 @@
-pragma solidity 0.8.4;  //Do not change the solidity version as it negativly impacts submission grading
+pragma solidity 0.8.4; //Do not change the solidity version as it negativly impacts submission grading
 // SPDX-License-Identifier: MIT
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "./YourToken.sol";
 
 contract Vendor is Ownable {
+    uint256 public constant tokensPerEth = 100;
 
-  //event BuyTokens(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
+    event BuyTokens(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
+    event SellTokens(
+        address seller,
+        uint256 amountOfETH,
+        uint256 amountOfTokens
+    );
 
-  YourToken public yourToken;
+    YourToken public yourToken;
 
-  constructor(address tokenAddress) {
-    yourToken = YourToken(tokenAddress);
-  }
+    constructor(address tokenAddress) {
+        yourToken = YourToken(tokenAddress);
+    }
 
-  // ToDo: create a payable buyTokens() function:
+    // ToDo: create a payable buyTokens() function:
+    function buyTokens() public payable {
+        require(msg.value > 0, "Send ETH to buy some tokens");
+        uint256 amountOfTokens = tokensPerEth * msg.value;
+        yourToken.transfer(msg.sender, amountOfTokens);
+        emit BuyTokens(msg.sender, msg.value, amountOfTokens);
+    }
 
-  // ToDo: create a withdraw() function that lets the owner withdraw ETH
+    // ToDo: create a withdraw() function that lets the owner withdraw ETH
+    function withdraw() public onlyOwner {
+        uint256 ownerBalance = address(this).balance;
+        require(ownerBalance > 0, "Owner has not balance to withdraw");
 
-  // ToDo: create a sellTokens(uint256 _amount) function:
+        (bool sent, ) = msg.sender.call{value: address(this).balance}("");
+        require(sent, "Failed to send user balance back to the owner");
+    }
 
+    // ToDo: create a sellTokens(uint256 _amount) function:
+    function sellTokens(uint256 tokenAmountToSell) public {
+        // Check that the requested amount of tokens to sell is more than 0
+        require(
+            tokenAmountToSell > 0,
+            "Specify an amount of token greater than zero"
+        );
+
+        // Check that the user's token balance is enough to do the swap
+        uint256 userBalance = yourToken.balanceOf(msg.sender);
+        require(
+            userBalance >= tokenAmountToSell,
+            "Your balance is lower than the amount of tokens you want to sell"
+        );
+
+        // Check that the Vendor's balance is enough to do the swap
+        uint256 amountOfETHToTransfer = tokenAmountToSell / tokensPerEth;
+        uint256 ownerETHBalance = address(this).balance;
+        require(
+            ownerETHBalance >= amountOfETHToTransfer,
+            "Vendor has not enough funds to accept the sell request"
+        );
+
+        bool sent = yourToken.transferFrom(
+            msg.sender,
+            address(this),
+            tokenAmountToSell
+        );
+        require(sent, "Failed to transfer tokens from user to vendor");
+
+        (sent, ) = msg.sender.call{value: amountOfETHToTransfer}("");
+        require(sent, "Failed to send ETH to the user");
+        emit SellTokens(msg.sender, amountOfETHToTransfer, tokenAmountToSell);
+    }
 }
--- packages/hardhat/deploy/01_deploy_vendor.js
+++ packages/hardhat/deploy/01_deploy_vendor.js
@@ -11,30 +11,36 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
   const yourToken = await ethers.getContract("YourToken", deployer);
 
   // Todo: deploy the vendor
-  // await deploy("Vendor", {
-  //   from: deployer,
-  //   args: [yourToken.address], // Learn more about args here: https://www.npmjs.com/package/hardhat-deploy#deploymentsdeploy
-  //   log: true,
-  // });
-  //
-  // const vendor = await ethers.getContract("Vendor", deployer);
+  await deploy("Vendor", {
+    from: deployer,
+    args: [yourToken.address], // Learn more about args here: https://www.npmjs.com/package/hardhat-deploy#deploymentsdeploy
+    log: true,
+  });
+
+  const vendor = await ethers.getContract("Vendor", deployer);
+  const deployerBalance = await yourToken.balanceOf(deployer);
+  const formatedDeployerBalance = +ethers.utils.formatEther(deployerBalance);
 
   // Todo: transfer the tokens to the vendor
-  // console.log("\n 🏵  Sending all 1000 tokens to the vendor...\n");
-  //
-  // const transferTransaction = await yourToken.transfer(
-  //   vendor.address,
-  //   ethers.utils.parseEther("1000")
-  // );
+  console.log(
+    `\n 🏵  Sending all ${formatedDeployerBalance} tokens to the vendor...\n`
+  );
 
-  //console.log("\n    ✅ confirming...\n");
-  //await sleep(5000); // wait 5 seconds for transaction to propagate
+  const transferTransaction = await yourToken.transfer(
+    vendor.address,
+    deployerBalance
+  );
+
+  console.log("\n    ✅ confirming...\n");
+  await sleep(5000); // wait 5 seconds for transaction to propagate
 
   // ToDo: change address to your frontend address vvvv
-  // console.log("\n 🤹  Sending ownership to frontend address...\n")
-  // const ownershipTransaction = await vendor.transferOwnership("** YOUR FRONTEND ADDRESS **");
-  // console.log("\n    ✅ confirming...\n");
-  // const ownershipResult = await ownershipTransaction.wait();
+  console.log("\n 🤹  Sending ownership to frontend address...\n");
+  const ownershipTransaction = await vendor.transferOwnership(
+    "0x04FAf55B9ed829F4c42ea3241761fde7A4F43630"
+  );
+  console.log("\n    ✅ confirming...\n");
+  const ownershipResult = await ownershipTransaction.wait();
 
   // ToDo: Verify your contract with Etherscan for public chains
   // if (chainId !== "31337") {
